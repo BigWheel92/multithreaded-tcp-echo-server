8dfe4e3a9b5f0354e78849a778b4538a09601ff5
Saad Farooq <saad92.sf@gmail.com>
2020-04-28 02:38:35 +0500

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <iostream>
+#include <arpa/inet.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  if (argc<2)
+  {
+     cout<<"Please pass the ip address of the server as well as the port on which the server is listening."<<endl;
+     return 1;
+  }
+
+ sockaddr_in remote_server;
+ int sock_fd;
+
+ if ( (sock_fd= socket(AF_INET, SOCK_STREAM, 0) ) ==-1) 
+ {
+    perror("socket: ");
+    exit(-1);
+ }
+
+ remote_server.sin_family=AF_INET;
+ remote_server.sin_port= htons(atoi(argv[2])); 
+ remote_server.sin_addr.s_addr= inet_addr(argv[1]);
+ bzero(&remote_server.sin_zero, 8);
+ 
+
+ if (connect(sock_fd, (sockaddr*) &remote_server, sizeof(sockaddr))==-1)
+ {
+   perror("connect: ");
+   exit(-1);
+ }
+
+ char userInput[100]="";
+
+ while (strcmp(userInput, "exit")!=0)
+ {
+   cout<<"Please enter the input to be sent to the server: ";
+   cin.getline(userInput, 99);
+
+   int bytes_sent= send(sock_fd, userInput, strlen(userInput)+1, 0);
+   if (bytes_sent==-1)
+   {
+    perror("send: ");
+    close(sock_fd);
+    exit(-1);
+   }
+
+   char recv_message[100];
+   int bytes_recv=recv(sock_fd, recv_message,  100, 0);
+   if (bytes_recv==-1)
+   {
+    perror("recv: ");
+    close(sock_fd);
+    exit(-1);
+   }
+
+   cout<<"The server sent: "<<recv_message<<endl;
+
+ }//end of while loop
+
+ close(sock_fd);
+
+}
---
+++ server.cpp
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <iostream>
+using namespace std;
+
+struct ThreadData
+{
+  int fd;
+  sockaddr_in client;
+};
+
+void* serverThread(void* param)
+{
+   ThreadData *clientData= ( (ThreadData*) param);
+
+   char clientInput[100]="";
+   
+   while (strcmp(clientInput, "exit")!=0)
+   {
+     int bytes_recv= recv(clientData->fd, clientInput, 100, 0);
+     if (bytes_recv==-1)
+     {
+        perror("recv");
+        close(clientData->fd);
+        break;
+     }
+ 
+     cout<<"Data Received from client ("<<inet_ntoa(clientData->client.sin_addr)<<"):  "<<clientInput<<endl<<endl;
+     
+     int bytes_sent=send(clientData->fd, clientInput,  strlen(clientInput)+1, 0);
+     if (bytes_sent==-1)
+     {
+        perror("send");
+        close(clientData->fd);
+        break;
+     }
+
+   }
+  
+  cout<<"Client ("<<inet_ntoa(clientData->client.sin_addr)<<") disconnected."<<endl<<endl;
+  delete clientData;
+  pthread_exit(NULL);
+
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc==1)
+  {
+     cout<<"Please pass the port number on which you want the server to listen."<<endl;
+     return 1;
+  }
+  
+  int sock_fd, client_fd;
+  sockaddr_in server, client;
+
+  if ((sock_fd= socket(AF_INET, SOCK_STREAM, 0))==-1)
+  {
+     perror("socket: ");
+     exit(-1);
+  }
+
+ server.sin_family= AF_INET;
+ server.sin_port= htons(atoi(argv[1]));
+ server.sin_addr.s_addr= INADDR_ANY;
+ bzero(&server.sin_zero, 8);
+
+ int len=sizeof(sockaddr_in);
+ 
+ if ((bind(sock_fd, (sockaddr*) &server, len))==-1)
+ {
+  perror("bind:");
+  exit(-1);
+ }
+
+ if (listen(sock_fd, 10)==-1)
+ {
+       perror("listen:");
+       exit(-1);
+ }
+
+ while (true)
+ {
+    ThreadData *newClient=new ThreadData;
+   
+   if( (newClient->fd=accept(sock_fd, (sockaddr*) &(newClient->client), (socklen_t*) &len) )==-1)
+   {
+	perror("accept:");
+	exit(-1);
+   }
+   
+  pthread_t tid=0;
+  if ( (pthread_create(&tid, NULL, &serverThread, newClient))==-1)
+  {
+    perror("thread");
+    continue;
+  }  
+
+  pthread_detach(tid);
+
+ }//end of while loop
+
+}
